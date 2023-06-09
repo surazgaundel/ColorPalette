22fbd46bdf85707ae7057ce3e390106e2b466307
surazgaundel <gaundelsuraz99@gmail.com>
2023-06-09 18:54:19 +0530

    Styling with Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Values from 'values.js'
-import './App.css'
-import SingleColor from '../SingleColor'
+import './index.css'
+import SingleColor from './SingleColor'
 
 function App() {
   const [color, setColor]=useState('')
@@ -24,19 +24,19 @@ function App() {
     setColor(e.target.value);
   }
   return (
-    <>
-    <form onSubmit={submitHandler}>
-      <h1>Color Palette</h1>
+    <div className='flex flex-col items-center mt-3'>
+      <h1 className='text-3xl '>Color Palette</h1>
+    <form className='mt-2 border border-slate-300 rounded-md p-3' onSubmit={submitHandler}>
       <input type="color" name="colorPalette" onChange={handleChange} value={color} autoComplete='color'/>
-      <input type="text" name="hexColor" onChange={handleChange} value={color} placeholder='#fff000'/>
-      <button type='submit' style={{backgroundColor:`${color}`}}>Submit</button>
+      <input type="text" name="hexColor" onChange={handleChange} value={color} placeholder='#fff000' className='border border-1 border-black mx-3 rounded-sm'/>
+      <button type='submit' className='border py-1 px-2 rounded-sm border-1 border-black 'style={{backgroundColor:`${color}`}}>Submit</button>
     </form>
-    <div>
+    <div className="grid grid-cols-4 w-full mt-2">
       {list.map((item,index)=>{
         return (<SingleColor key={index} {...item} index={index}/>)
       })}
     </div>
-    </>
+    </div>
   )
 }
 
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import rgbToHex from './rgbToHex';
 
 export default function SingleColor({rgb,weight,index}) {
