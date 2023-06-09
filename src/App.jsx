809baa1dcf133e4e19a395d3298e761255222e27
surazgaundel <gaundelsuraz99@gmail.com>
2023-06-09 19:43:16 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import SingleColor from './SingleColor'
 
 function App() {
   const [color, setColor]=useState('')
-  const [errpor, setError]=useState(false); 
-  const [list,setList]=useState([])
+  const [error, setError]=useState(false); 
+  const [list,setList]=useState(new Values('#fff000').all(10));
 
   const submitHandler=(e)=>{
     e.preventDefault();
@@ -24,14 +24,14 @@ function App() {
     setColor(e.target.value);
   }
   return (
-    <div className='flex flex-col items-center mt-3'>
-      <h1 className='text-3xl '>Color Palette</h1>
+    <div className='flex flex-col items-center mt-3 h-screen'>
+      <h1 className='text-3xl'>Color Palette</h1>
     <form className='mt-2 border border-slate-300 rounded-md p-3' onSubmit={submitHandler}>
       <input type="color" name="colorPalette" onChange={handleChange} value={color} autoComplete='color'/>
-      <input type="text" name="hexColor" onChange={handleChange} value={color} placeholder='#fff000' className='border border-1 border-black mx-3 rounded-sm'/>
+      <input type="text" name="hexColor" onChange={handleChange} value={color} placeholder='#fff000' className={`border border-1 border-black mx-3 rounded-sm ${error? 'error':''}`}/>
       <button type='submit' className='border py-1 px-2 rounded-sm border-1 border-black 'style={{backgroundColor:`${color}`}}>Submit</button>
     </form>
-    <div className="grid grid-cols-4 w-full mt-2">
+    <div className="grid grid-cols-5 w-full mt-2">
       {list.map((item,index)=>{
         return (<SingleColor key={index} {...item} index={index}/>)
       })}
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,19 +1,33 @@
 /* eslint-disable react/prop-types */
-import  { useState } from 'react'
+import  { useEffect, useState } from 'react'
 import rgbToHex from './rgbToHex';
 
 export default function SingleColor({rgb,weight,index}) {
     const[alert,setAlert]=useState(false)
+
     const bcg=rgb.join(',')
     const hex=rgbToHex(...rgb).toUpperCase();
     const style={
         backgroundColor:`rgb(${bcg})`,
-        color:`${index>10 && 'white'}`
+        color:`${index>10 && '#fff'}`,
+        padding:'2em'
     }
+    useEffect(()=>{
+      const timeout=setTimeout(()=>{
+        setAlert(false);
+      },2000)
+      return()=>clearTimeout(timeout)
+    })
   return (
-    <article style={style}>
+    <article style={style} className='relative'
+    onClick={()=>{
+      setAlert(true);
+      navigator.clipboard.writeText(hex);
+    }}
+    >
         <p>{weight}%</p>
         <p>{hex}</p>
+        {alert && <p className='absolute inset-0 text-center border-2 border-slate-500 rounded-sm'>Copied to clipboard</p>}
     </article>
   )
 }
